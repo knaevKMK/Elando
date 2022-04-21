dd0514c286935ff37fc6ee8c4fdd7355ac9552ec
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-04-21 23:15:09 +0300

    init test project

---
+++ Elando.Test/AppTest.cs
@@ -0,0 +1,15 @@
+using IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elando.Test
+{
+    [TestClass]
+    public class AppTest
+    {
+      
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
