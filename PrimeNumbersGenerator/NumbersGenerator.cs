77fa5faef33f85ac79e12e3a37610cf26f01422c
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-04-23 20:11:36 +0300

    commit after amend

---
+++ PrimeNumbersGenerator/NumbersGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace Generator
+{
+    public class NumbersGenerator
+    {
+        public long[] Generate(int Length)
+        {
+            var result = new long[Length];
+            int index = 0;
+            long checkNum = 2;
+            while (index < Length)
+            {
+                if (isPrime(checkNum))
+                {
+                    result[index++] = checkNum;
+                }
+                checkNum++;
+            }
+            return result;
+        }
+
+        private bool isPrime(long num)
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                {
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            return isPrime;
+        }
+    }
+}
